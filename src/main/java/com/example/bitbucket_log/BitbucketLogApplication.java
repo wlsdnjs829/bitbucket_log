c863245ccb7bbb91f1897155858ed84af22c959f
이진원 <ljw0829@midasit.com>
2022-07-09 22:53:52 +0900

    - feat : bitbucket log 예시 소스

---
+++ src/main/java/com/example/bitbucket_log/BitbucketLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitbucket_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbucketLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitbucketLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bitbucket_log/BitbucketLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitbucket_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitbucketLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
